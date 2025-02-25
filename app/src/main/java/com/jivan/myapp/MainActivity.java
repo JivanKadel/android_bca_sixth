1413d5a65d2589388d4940f09276def47850e77b
Jivan Kadel <jivankadel256@gmail.com>
2025-02-25 09:18:26 +0545

    Sent and received data via Intent

--- app/src/main/java/com/jivan/myapp/MainActivity.java
+++ app/src/main/java/com/jivan/myapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.jivan.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private Button loginButton;
+    private static final String IS_LOGGED_IN = "IS_LOGGED_IN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +21,25 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        loginButton = findViewById(R.id.btnLogin);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                intent.putExtra(IS_LOGGED_IN, true);
+                startActivity(intent);
+            }
         });
     }
 }
--- app/src/main/java/com/jivan/myapp/SignupActivity.java
+++ app/src/main/java/com/jivan/myapp/SignupActivity.java
@@ -1,6 +1,10 @@
 package com.jivan.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,15 +13,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SignupActivity extends AppCompatActivity {
-    public void onCreate(Bundle savedInstanceState){
+
+    Button homeButton;
+
+    public void onCreate(Bundle savedInstanceState) {
         EdgeToEdge.enable(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.signupPage), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        // Get intent data
+        boolean IS_LOGGED_IN = getIntent().getBooleanExtra("IS_LOGGED_IN", false);
+        Toast.makeText(getApplicationContext(), IS_LOGGED_IN ? "Logged in" : "Not Logged In", Toast.LENGTH_LONG).show();
+
+        homeButton = findViewById(R.id.btnSubmit);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignupActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
