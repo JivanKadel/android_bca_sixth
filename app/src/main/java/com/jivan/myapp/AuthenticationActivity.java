b09349b00de0d0e8c3fcdc9cb85ae7d81455b20b
Jivan Kadel <jivankadel256@gmail.com>
2025-03-02 09:20:43 +0545

    handle auth, auth_fragments

---
+++ app/src/main/java/com/jivan/myapp/AuthenticationActivity.java
@@ -0,0 +1,57 @@
+package com.jivan.myapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    Button btnLogin, btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_authentication);
+
+        btnLogin = findViewById(R.id.btn_login);
+        btnSignup = findViewById(R.id.btn_signup);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // login, signup btn click listener
+        btnLogin.setOnClickListener(view -> {
+            replaceFragment(new LoginFragment());
+        });
+
+        btnSignup.setOnClickListener(view -> {
+            replaceFragment(new SignupFragment());
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.auth_frame, fragment);
+        fragmentTransaction.commit();
+    }
+
+}
