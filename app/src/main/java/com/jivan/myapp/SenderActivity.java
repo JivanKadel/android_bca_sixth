cb062217f81723cbd101b6afbef4209c38e60177
Jivan Kadel <jivankadel256@gmail.com>
2025-03-10 09:07:28 +0545

    Show received address (startActivityForResult())

--- app/src/main/java/com/jivan/myapp/SenderActivity.java
+++ app/src/main/java/com/jivan/myapp/SenderActivity.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,6 +17,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class SenderActivity extends AppCompatActivity {
     Button btnSubmit;
     EditText etName, etPhone;
+    TextView tvAddress;
+    private final int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +27,10 @@ public class SenderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sender);
 
         btnSubmit = findViewById(R.id.btnSend);
-        etName = findViewById(R.id.editTextText);
+        etName = findViewById(R.id.editTextName);
         etPhone = findViewById(R.id.editTextPhone);
 
+        tvAddress = findViewById(R.id.textViewAddress);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -41,10 +46,23 @@ public class SenderActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), ReceiverActivity.class);
-                intent.putExtra("Name", etName.getText().toString());
-                intent.putExtra("Phone", etPhone.getText().toString());
-                startActivity(intent);
+                intent.putExtra("name", etName.getText().toString());
+                intent.putExtra("phone", etPhone.getText().toString());
+//                startActivity(intent);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                String address = data.getStringExtra("address");
+                tvAddress.setText(address);
+            }
+        }
     }
 }
