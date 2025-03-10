7db6678c78718ee34d4a3c085045cfc15973e479
Jivan Kadel <jivankadel256@gmail.com>
2025-03-10 09:06:34 +0545

    dialog box as fragment

--- app/src/main/java/com/jivan/myapp/ReceiverActivity.java
+++ app/src/main/java/com/jivan/myapp/ReceiverActivity.java
@@ -1,6 +1,11 @@
 package com.jivan.myapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,7 +16,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ReceiverActivity extends AppCompatActivity {
 
+    Button btnSubmitBack, btnShowDialog;
+    EditText etAddress;
     TextView tvName, tvPhone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +30,12 @@ public class ReceiverActivity extends AppCompatActivity {
         tvName = findViewById(R.id.textViewName);
         tvPhone = findViewById(R.id.textViewPhone);
 
-        String name = getIntent().getStringExtra("Name");
-        String phone = getIntent().getStringExtra("Phone");
+        etAddress = findViewById(R.id.editTextAddress);
+        btnSubmitBack = findViewById(R.id.btnSubmitBack);
+        btnShowDialog = findViewById(R.id.btnShowDialogFragment);
+
+        String name = getIntent().getStringExtra("name");
+        String phone = getIntent().getStringExtra("phone");
 
 
         tvName.setText(String.format("Name: %s", name));
@@ -33,6 +45,32 @@ public class ReceiverActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnSubmitBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (etAddress != null && etAddress.getText() != null) {
+                    String address = etAddress.getText().toString();
+
+                    Intent intent = new Intent();
+                    intent.putExtra("address", address);
+                    setResult(Activity.RESULT_OK, intent);
+                    finish();
+                }
+            }
+        });
+
+        btnShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDialogFragment myDialogFragment = new MyDialogFragment();
+                myDialogFragment.show(getSupportFragmentManager(), "MyDialog");
+            }
         });
     }
 }
