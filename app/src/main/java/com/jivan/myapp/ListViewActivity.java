5d8263b9de716a0c2e85ed0f6db208f917245fef
Jivan Kadel <jivankadel256@gmail.com>
2025-03-11 09:15:43 +0545

    List view in android

---
+++ app/src/main/java/com/jivan/myapp/ListViewActivity.java
@@ -0,0 +1,40 @@
+package com.jivan.myapp;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ListViewActivity extends AppCompatActivity {
+    String[] courses = {"BCA", "CSIT", "BBA", "BIM", "BBS", "BTECH", "MSc. CSIT", "MCA", "MBS", "MBA"};
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list_view);
+
+        listView = findViewById(R.id.listViewCourses);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_activated_1, courses);
+        listView.setAdapter(adapter);
+    }
+}
