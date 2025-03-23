f609f8ab9cfb60ca76ae8753777c99ab0666f4a9
Jivan Kadel <jivankadel256@gmail.com>
2025-03-23 09:09:31 +0545

    make the class receive args via constructor

--- app/src/main/java/com/jivan/myapp/MyDialogFragment.java
+++ app/src/main/java/com/jivan/myapp/MyDialogFragment.java
@@ -11,27 +11,37 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
  public class MyDialogFragment extends DialogFragment{
+     private String title = "Confirmation", message = "Are you sure you want to continue?", positiveMessage = "Yes";
+     public MyDialogFragment(String title, String message, String positiveMessage){
+         this.title = title;
+         this.message = message;
+         this.positiveMessage = positiveMessage;
+     }
+     public MyDialogFragment(){
+
+     }
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
 
 
         AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
-        builder.setTitle("Confirmation").setMessage("Are you sure you want to continue?")
+        builder.setTitle(title).setMessage(message)
                 .setIcon(R.drawable.ic_launcher_background)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                .setPositiveButton(positiveMessage, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int i) {
-                        Toast.makeText(requireActivity(), "Continue...", Toast.LENGTH_LONG).show();
-                        dialog.dismiss();
-                    }
-                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int i) {
-                        Toast.makeText(requireActivity(), "Cancelling...", Toast.LENGTH_LONG).show();
+//                        Toast.makeText(requireActivity(), "Continue...", Toast.LENGTH_LONG).show();
                         dialog.dismiss();
                     }
                 });
+//                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int i) {
+//                        Toast.makeText(requireActivity(), "Cancelling...", Toast.LENGTH_LONG).show();
+//                        dialog.dismiss();
+//                    }
+//                });
 
         return builder.create();
     }
